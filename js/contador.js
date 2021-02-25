a11a30e628adca7d391953cb3f081c78ca6cee48
EduardoDos-SantosP <66569234+EduardoDos-SantosP@users.noreply.github.com>
2021-02-25 18:02:50 -0300

    Create contador.js

---
+++ js/contador.js
@@ -0,0 +1,14 @@
+//Quando clicado o botao de incio, sera criado tres variaveis, n = xontador das questoes, correto = contador de acertos e questao = indicador de se a questao esta correta: correta = 1, errada = 0
+var n = 0;
+var correto = 0;
+var questao = [90];
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// quando respondida uma questao e o botao de proxima for clicado, o contador analisara se a questao esta correta, vendo o valor da variavel questao, se esta estiver com o valor = 1 a variavel correta tera a adicao de mais 1. 
+while( n<= 90){
+if (questao[n] = 1){
+    correto = correto + 1;
+}
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
